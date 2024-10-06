739720ab8b04d1e11d161237781273258ed9c107
JoshuaMacklin <Joshua.macklin@pm.me>
2024-10-05 21:55:45 -0700

    Part 3: Class Features

--- script.js
+++ script.js
@@ -41,11 +41,40 @@ class Character {
     }
 }
 
-const robin = new Character("Robin");
+class Adventurer extends Character {
+    constructor (name, role) {
+      super(name);
+      // Adventurers have specialized roles.
+      this.role = role;
+      // Every adventurer starts with a bed and 50 gold coins.
+      this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout() {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+    }
+    sleep() {
+        console.log(`${this.name} takes a nap...`);
+        super.roll()
+    }
+}
+
+class Companion extends Character {
+    constructor (name, role) {
+        super(name);
+        this.role = role;
+    }
+    runAhead() {
+        console.log(`${this.name} runs ahead...`);
+        super.roll()
+    }
+}
+const robin = new Adventurer("Robin");
 robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
+robin.companion = new Companion("Leo");
 robin.companion.type = "Cat";
-robin.companion.companion = new Character("Frank");
+robin.companion.companion = new Companion("Frank");
 robin.companion.companion.type = "Flea";
 robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
@@ -56,5 +85,4 @@ robin.companion.roll()
 robin.companion.companion.roll()
 
 console.log("--------");
-
-
+robin.companion.companion.runAhead()

c079f261413c1881ffb689adc97f766157541061
JoshuaMacklin <Joshua.macklin@pm.me>
2024-10-05 20:42:00 -0700

    Part 2: Class Fantasy

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+let roll = function(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`)
+}
+
 const adventurer = {
     name: "Robin",
     health: 10,
     inventory: ["sword", "potion", "artifact"],
-    roll(mod = 0) {
-        const result = Math.floor(Math.random() * 20) + 1 + mod;
-        console.log(`${this.name} rolled a ${result}.`)
-    }
+    roll
 }
 
 adventurer.inventory.forEach((item)=>{
@@ -26,4 +28,33 @@ adventurer.companion.companion = {
 adventurer.roll()
 adventurer.roll()
 adventurer.roll()
+
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+    }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+console.log("--------");
+
+robin.roll()
+robin.companion.roll()
+robin.companion.companion.roll()
+
+console.log("--------");
+
 
